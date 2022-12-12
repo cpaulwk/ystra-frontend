741851f32c51e6ab5f44bf5d3e17452f8e58a950
Seydou Camara <Seydoucamara1990@gmail.com>
2022-12-12 16:03:21 +0100

    homepageV1

---
+++ screens/HomeScreen.js
@@ -0,0 +1,51 @@
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
+
+export default function HomePage({ navigation }) {
+  return (
+    <KeyboardAvoidingView
+      style={{ ...styles.container, justifyContent: "flex-start" }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <Image
+        style={styles.image}
+        source={require("../assets/homescreen-background.jpg")}
+      />
+      <Text style={styles.text}>Welcome to Ystra</Text>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  title: {
+    width: "80%",
+    fontSize: 38,
+    fontWeight: "600",
+  },
+  text: {
+    fontSize: 30,
+    opacity: 0.65,
+    fontWeight: "bold",
+    color: "black",
+    position: "absolute",
+    bottom: 530,
+  },
+});
