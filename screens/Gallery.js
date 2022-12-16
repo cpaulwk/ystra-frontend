063acaf9832875fbc36dd2c31b96e636e5997968
Paul Chow <cpaulwenkai@gmail.com>
2022-12-16 15:37:34 +0100

    add dispatch and login function for reducer

--- screens/Gallery.js
+++ screens/Gallery.js
@@ -1,16 +1,17 @@
-import { View,Text } from "react-native";
+import { View, Text } from "react-native";
 
 import { useSelector } from "react-redux";
-
+import tw from "twrnc";
 
 export default function Gallery({ navigation }) {
-    
-    const user=useSelector(state=> state.user.value);
+  const user = useSelector((state) => state.user.value);
 
-    return (
-        <View>
-            <Text>Gallery</Text>
-            <Text>{user.userName}  {user.token}</Text>
-        </View>
-    );
+  return (
+    <View style={tw`flex-1 justify-center items-center`}>
+      <Text>Gallery</Text>
+      <Text>Username: {user.userName}</Text>
+      <Text>Token: {user.token}</Text>
+    </View>
+  );
 }
+
