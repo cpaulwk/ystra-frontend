a449f336addc0eb831d9ad112fcedc1c974ef829
Paul Chow <cpaulwenkai@gmail.com>
2023-02-10 22:38:41 +0100

    add variable for style automation w/ or w/o icon for InputFieldWithIcon.js and create ReturnButton.js component

--- components/uikit/InputFieldWithIcon.js
+++ components/uikit/InputFieldWithIcon.js
@@ -3,25 +3,31 @@ import FontAwesome from "react-native-vector-icons/FontAwesome";
 import tw from "twrnc";
 
 export default function InputFieldWithIcon(props) {
+  let iconStyle =
+    "border border-[#9ca3af] bg-white p-3 pl-13 opacity-90 w-full rounded-2.5 text-4";
+  if (!props.iconName)
+    iconStyle =
+      "border border-[#9ca3af] bg-white p-3 opacity-90 w-full rounded-2.5 text-4";
+
   return (
     <View style={tw`flex-row items-center mt-5 mb-2`}>
       <TextInput
         placeholder={props.placeholder}
         value={props.value}
-        // ********
         onChangeText={(value) => {
           props.onChangeText(value, props.placeholder);
         }}
-        // ********
-        style={tw`border border-[#9ca3af] bg-white p-3 pl-13 opacity-90 w-full rounded-2.5 text-4`}
+        style={tw`${iconStyle}`}
         autoCapitalize="none"
         secureTextEntry={props.secureTextEntry}
       />
-      <View
-        style={tw`absolute border-r border-[#AFAFAF] flex justify-center items-center rounded-l-2.5 h-full aspect-square pl-1`}
-      >
-        <FontAwesome name={props.iconName} size={20} />
-      </View>
+      {props.iconName && (
+        <View
+          style={tw`absolute border-r border-[#AFAFAF] flex justify-center items-center rounded-l-2.5 h-full aspect-square pl-1`}
+        >
+          <FontAwesome name={props.iconName} size={20} />
+        </View>
+      )}
     </View>
   );
 }
